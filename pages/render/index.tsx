6f9f03f55f60ae7933c60f6f09787fc37b02b076
Daria Metelitsa <dariametelitsa@gmail.com>
2024-10-16 17:31:26 +0300

    added revalidate on demand

--- pages/render/index.tsx
+++ pages/render/index.tsx
@@ -1,15 +1,43 @@
 import { getLayout } from "components/Layout/BaseLayout/BaseLayout";
 import styled from "styled-components";
 import React, { useState } from "react";
+import fs from 'fs/promises'
+import * as path from "node:path";
 
-const render = () => {
-return (
+export const getStaticProps = async () => {
+  const getParsedData = async (): Promise<{title: string}> => {
+    const filePath = path.join(process.cwd(), 'public', 'staticData.json')
+    try {
+      const jsonData = await fs.readFile(filePath)
+      return JSON.parse(jsonData.toString())
+    } catch (error) {
+      return {title: 'No title'}
+    }
+  }
+
+  const {title} = await getParsedData()
+
+  return {
+    props: {
+      title
+    }
+  }
+}
+
+type PropsType = {
+  title: string
+}
+
+const render = (props: PropsType) => {
+  console.log(props)
+  return (
     <Block>
-        <Input render={(value) => {
-            return (<div>{value}</div>)
-        }}/>
+      <p>Title: {props.title}</p>
+      <Input render={(value) => {
+        return (<div>{value}</div>)
+      }}/>
     </Block>
-)
+  )
 }
 
 render.getLayout = getLayout
@@ -22,15 +50,15 @@ const Block = styled.div`
 `
 
 type InputProps = {
-    render: (value: string) => React.ReactElement
+  render: (value: string) => React.ReactElement
 }
 const Input = (props: InputProps) => {
 
-    const [value, setValue] = useState('');
-    return(
-        <>
-        <input type={"text"} value={value} onChange={(e) => setValue(e.currentTarget.value)} />
-            {props.render(value)}
-        </>
-    )
+  const [value, setValue] = useState('');
+  return(
+    <>
+      <input type={"text"} value={value} onChange={(e) => setValue(e.currentTarget.value)} />
+      {props.render(value)}
+    </>
+  )
 }
